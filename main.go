8af86194bd0dff6a354f8cde6a9c157c0766563e
Luan Santos <cfcluan@gmail.com>
2014-12-08 21:59:02 -0800

    Add create command
    [#83526122]

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+
+	"github.com/luan/tiego/commands"
+	"github.com/tmtk75/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tiego"
+	app.Usage = "manages tiego workstations and shell sessions"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "teapot, t",
+			Value:  "http://127.0.0.1:8080",
+			Usage:  "address of the Teapot to use",
+			EnvVar: "TEAPOT",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:      "create",
+			ShortName: "c",
+			Usage:     "creates a workstation",
+			Action:    commands.CreateWokstation,
+			Args:      "<name>",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "docker-image, d",
+					Value: "docker:///ubuntu#trusty",
+					Usage: "docker image available on hub.docker.com",
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
